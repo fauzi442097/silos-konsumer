49c44ca1cfb7d02d767ff57a3843c0d963df664b
ilhamnurrohman74 <ilhamnurrohman74@gmail.com>
2023-06-05 09:46:52 +0700

    update

--- src/app/(pages)/new_entry/page.js
+++ src/app/(pages)/new_entry/page.js
@@ -2,13 +2,19 @@
 import React from 'react'
 import Card from '@/app/components/Card'
 import MyDatatable from '@/app/components/datatable/MyDatatable'
+import Button from '@/app/components/Button'
 
 const NewEntry = () => {
   return (
     <>
       <h1 className='text-3xl font-semibold dark:text-white'> New Entry </h1>
       <Card>
-        <MyDatatable/>
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <div>
+            <Button className="mx-2"> Entry Data </Button>
+          </div>
+        </div>
+        <MyDatatable />
       </Card>
     </>
   )
