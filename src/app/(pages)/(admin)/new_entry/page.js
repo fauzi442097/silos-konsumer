45879cb109fe0f96f1e9a04813fc3630046aa019
ilhamnurrohman74 <ilhamnurrohman74@gmail.com>
2023-06-12 13:48:30 +0700

    update new entry

---
+++ src/app/(pages)/(admin)/new_entry/page.js
@@ -0,0 +1,140 @@
+'use client'
+import React, { useEffect, useState } from "react"
+
+import { Button, Card, MyDataTable, CheckboxTable, LoadingTable, DropdownButton } from '@/app/components';
+import { HiPencilSquare, HiPaperAirplane, HiPencil, HiOutlineClipboardDocumentList } from "react-icons/hi2";
+
+const newEntry = () => {
+
+    const [listNewEntry, setListNewEntry] = useState([]);
+    useEffect(() => {
+        const getNewEntry = async () => {
+            const res = await fetch('https://dummyjson.com/products');
+            const data = await res.json();
+            setListNewEntry(data.products);
+        }
+        getNewEntry();
+    }, []);
+
+    const columns = [
+        {
+            name: 'Action',
+            cell: (row, index, column, id) => <DropdownButton>
+                <li>
+                    <a href="#" className="flex items-center px-3 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <HiPencilSquare className="w-5 h-5 mx-1"></HiPencilSquare>
+                        <span className="ml-3">
+                            Ubah Data
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center px-3 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <HiOutlineClipboardDocumentList className="w-5 h-5 mx-1"></HiOutlineClipboardDocumentList>
+                        <span className="ml-3">
+                            Slik
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center px-3 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <HiPencil className="w-5 h-5 mx-1"></HiPencil>
+                        <span className="ml-3">
+                            Checklist Dokumen
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center px-3 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <HiPencil className="w-5 h-5 mx-1"></HiPencil>
+                        <span className="ml-3">
+                            Edit Biaya Lainnya
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center px-3 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <HiPaperAirplane className="w-5 h-5 mx-1"></HiPaperAirplane>
+                        <span className="ml-3">
+                            Kirim Prospek
+                        </span>
+                    </a>
+                </li>
+            </DropdownButton>,
+            ignoreRowClick: true,
+            allowOverflow: true,
+            button: true,
+        },
+        {
+            name: 'Nama Nasabah',
+            selector: (row) => row.brand,
+            cellExport: row => row.brand,
+            sortable: true,
+            center: false,
+        },
+        {
+            name: 'Alamat',
+            selector: (row) => row.description,
+            cellExport: row => row.description,
+            sortable: true,
+            center: false,
+        },
+        {
+            name: 'Produk',
+            selector: (row) => row.title,
+            cellExport: row => row.title,
+            sortable: true,
+            center: false,
+        },
+        {
+            name: 'Plafon',
+            selector: (row) => row.price,
+            cellExport: row => row.price,
+            sortable: true,
+            center: false,
+        },
+        {
+            name: 'Jangka Waktu',
+            selector: (row) => row.stock,
+            cellExport: row => row.stock,
+            sortable: true,
+            center: false,
+        },
+        {
+            name: 'Angsuran',
+            selector: (row) => row.rating,
+            cellExport: row => row.rating,
+            sortable: true,
+            center: false,
+        }
+    ];
+
+    return (
+        <>
+            <Card>
+                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        New Entry
+                    </h3>
+                </div>
+                <MyDataTable
+                    fixedHeader={true}
+                    // fixedHeaderScrollHeight="500px"
+                    columns={columns}
+                    data={listNewEntry}
+                    selectableRows={true}
+                    pagination={true}
+                    paginationPerPage={10}
+                    responsive={true}
+                    progressPending={false}
+                    selectableRowsComponent={CheckboxTable}
+                    progressComponent={<LoadingTable />}
+                    contextActions={<Button.LightPrimary onClick={() => alert('on progress')}>Hapus</Button.LightPrimary>}>
+                </MyDataTable>
+            </Card>
+        </>
+    )
+
+}
+
+export default newEntry
--- src/app/components/DropdownButton.js
+++ src/app/components/DropdownButton.js
@@ -1,55 +1,28 @@
 import React, { useState } from 'react'
-import { useButtonDropdown } from '../hooks/ButtonDropdownContext'
-import { useSidebar } from '../hooks/SidebarContext'
-import { motion, AnimatePresence } from "framer-motion"
 
 const DropdownButton = ({ children }) => {
-
-    const { openDropdown, toggleDropdown } = useButtonDropdown()
-    const { openSidebar, toggleSidebar, setOpenSideMenu } = useSidebar();
-    const [showDropdownButton, setShowDropdownButton] = useState({ button: false });
-
-    const setToggleDropdown = () => {
-        toggleDropdown();
-    }
-
-    const toggleButtonDropdown = (clicked) => {
-        const newObject = Object.keys(showDropdownButton)
-            .filter((key) => !key.includes(clicked))
-            .reduce((obj, key) => {
-                return Object.assign(obj, {
-                    [key]: false
-                });
-            }, {});
-
-        newObject[clicked] = !showDropdownButton[clicked];
-        setShowDropdownButton(newObject);
-    }
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <>
-            <button onClick={() => toggleButtonDropdown('button')} id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" className={` inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600`} type="button">
-                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg>
-            </button>
-
-            <div id="dropdownDots" className={`z-100 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 ${showDropdownButton.button ? 'block' : 'hidden'}`}>
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                </ul>
-                <div className="py-2">
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated link</a>
-                </div>
-                <AnimatePresence>
-                    {showDropdownButton.button}
-                </AnimatePresence>
+            <div className="relative inline-block text-left">
+                <button
+                    type="button"
+                    data-dropdown-toggle="dropdownDots"
+                    className="relative z-10 block p-2 text-gray-700 bg-white border border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg>
+                </button>
+                {isOpen && (
+                    <div className="absolute left-0 z-20 py-2 mt-2 origin-top-left bg-white rounded-md shadow-xl dark:bg-gray-800" style={{width: "14rem"}}>
+                        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">                            
+                                {children}
+                            </ul>
+                        </div>
+                    </div>
+                )}
             </div>
         </>
 
