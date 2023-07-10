6036064d2abaf70dff764d0ae04e2b25ec93e5c8
fauzi442097 <50788616+fauzi442097@users.noreply.github.com>
2023-07-10 12:03:12 +0700

    fetching data

--- src/app/(admin)/error.js
+++ src/app/(admin)/error.js
@@ -1,5 +1,6 @@
 'use client'
 
+import Alert from '@/components/Alert'
 import Button from '@/components/Button'
 import React from 'react'
 
@@ -7,7 +8,7 @@ const error = ({ error, reset }) => {
   return (
     <div className='sticky flex justify-center items-center flex-col w-full'>
         <div className='p-8'>
-          <h1>Something went wrong!</h1>
+          <h1> {error.name == 'Fetching Error' ? 'Failed to retrive data' : 'Something went wrong!'} </h1>
           <p className='text-danger text-xl'> {error.message || 'Terjadi kesalahan'} </p>
           <Button size={'sm'} onClick={reset}> Try Again </Button>
         </div>
