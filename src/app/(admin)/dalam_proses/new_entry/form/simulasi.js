a564e5b460252355c2f9e7bc30d3a3df41e5be6c
ilhamnurrohman74 <ilhamnurrohman74@gmail.com>
2023-07-21 09:23:26 +0700

    update

--- src/app/(admin)/dalam_proses/new_entry/form/simulasi.js
+++ src/app/(admin)/dalam_proses/new_entry/form/simulasi.js
@@ -1,34 +1,60 @@
 'use client'
 
-import React from "react"
+import React, { useState, useEffect } from "react"
 import MySelect from "@/components/Form/Select";
 import Input from "@/components/Form/Input";
+import { API } from "@/config/api";
+import MySwal from "@/components/Swal/MySwal";
 
 const options = [
     { value: "fox", label: "Fox" },
     { value: "Butterfly", label: "Butterfly" },
     { value: "Honeybee", label: "Honeybee" }
- ];
+];
 
 const Simulasi = () => {
+    const [produk, setProduk] = useState([]);
+
+    const getProduk = async () => {
+        const arr = [];
+        const response = await API.GET(`/master/list/product`);
+        if (response.status != 200) return MySwal.error(response.data.error)
+
+        let data = response.data;
+        data.map((item) => {
+            return arr.push({ value: item.id, label: item.prodName })
+        })
+
+        setProduk(arr);
+    }
+
+    const handleChangeProduk = value => {
+        console.log(value)
+        setProduk([value]);
+    };
+
+    useEffect(() => {
+        getProduk();
+    }, []);
+
     return (
         <>
-            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{gap: "30px"}}>
+            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Produk </label>
-                    <MySelect withSearch options={options} />
+                    <MySelect withSearch options={produk} value={produk} onChange={handleChangeProduk} />
                 </div>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Pekerjaan </label>
-                    <MySelect withSearch options={options}/>
+                    <MySelect withSearch options={options} />
                 </div>
                 <div style={{ width: "250px" }}>
                 </div>
             </div>
-            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{gap: "30px"}}>
+            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Status Menikah </label>
-                    <MySelect withSearch options={options}/>
+                    <MySelect withSearch options={options} />
                 </div>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Tanggal Lahir </label>
@@ -39,7 +65,7 @@ const Simulasi = () => {
                     <Input.Text></Input.Text>
                 </div>
             </div>
-            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{gap: "30px"}}>
+            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Suku Bunga (dalam %) </label>
                     <Input.Text readOnly></Input.Text>
@@ -53,7 +79,7 @@ const Simulasi = () => {
                     <p className="italic text-sm font-bold text-red-500 font-inter-extralight dark:text-white">Jangka waktu melebihi maksimal tenor pekerjaan</p>
                 </div>
             </div>
-            <div className="flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7" style={{gap: "30px"}}>
+            <div className="flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7" style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Bunga Promo (%) </label>
                     <Input.Text></Input.Text>
@@ -65,10 +91,10 @@ const Simulasi = () => {
                 <div style={{ width: "250px" }}>
                 </div>
             </div>
-            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{gap: "30px"}}>
+            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Asuransi </label>
-                    <MySelect withSearch options={options}/>
+                    <MySelect withSearch options={options} />
                 </div>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Rate Asuransi </label>
@@ -77,7 +103,7 @@ const Simulasi = () => {
                 <div style={{ width: "250px" }}>
                 </div>
             </div>
-            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{gap: "30px"}}>
+            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Gaji </label>
                     <Input.Currency
@@ -118,7 +144,7 @@ const Simulasi = () => {
                     />
                 </div>
             </div>
-            <div className="flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7" style={{gap: "30px"}}>
+            <div className="flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7" style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Total Penghasilan </label>
                     <Input.Text readOnly></Input.Text>
@@ -128,7 +154,7 @@ const Simulasi = () => {
                 <div style={{ width: "250px" }}>
                 </div>
             </div>
-            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{gap: "30px"}}>
+            <div className='flex flex-row justify-center gap-4 w-full md:flex-nowrap flex-wrap my-4 mb-7' style={{ gap: "30px" }}>
                 <div style={{ width: "250px" }}>
                     <label className='block mb-3'> Plafon Kredit yang diberikan </label>
                     <Input.Text></Input.Text>
--- src/components/Form/Select.js
+++ src/components/Form/Select.js
@@ -15,7 +15,7 @@ const MySelect = ({ withSearch = false, disabled = false, ...props}) => {
                         : "bg-white dark:bg-dark-depth2 dark:border-dark-depth2 hover:border-gray-400 focus:border-primary focus:ring focus:ring-primary/20"
                   }`
             ),
-            menu: "absolute z-40 w-full bg-white dark:bg-dark-depth2 shadow-lg dark:shadow-none dark:border-dark-depth2 border rounded-xl py-2 mt-1.5 transition duration-300",
+            menu: "absolute z-40 bg-white dark:bg-dark-depth2 shadow-lg dark:shadow-none dark:border-dark-depth2 border rounded-xl py-2 mt-1.5 transition duration-300",
             searchContainer: 'px-3 my-4',
             searchBox: 'rounded-lg border w-full px-9 py-2 focus:outline-none focus:ring-1 focus:ring-primary dark:focus:ring-primary-800 dark:bg-[#575b5f] dark:border-[#575b5f] dark:focus:ring-primary-800',
             listItem: ({ isSelected }) => (
