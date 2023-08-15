15da369035d37d501d75963e2966369aa37c807a
fauzi442097 <50788616+fauzi442097@users.noreply.github.com>
2023-08-15 16:17:12 +0700

    set card to server component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,9 @@
-'use client'
-
 import React from 'react'
-import { useTheme } from '../hooks/ThemeContext';
 import { cn } from '@/lib/utils';
 
 const Card = ({ children, className }) => {
-
-  const { theme } = useTheme()
-  const boxShadowCardStyle = theme == 'light' ? {
-    'boxShadow': '#c7cdc969 3px 0px 25px 0px'
-  } : {}
-
   return (
-    <div style={boxShadowCardStyle} className={cn(['card rounded-2xl bg-white dark:bg-dark-depth1 dark:text-grey dark:shadow-none w-full', className])}>
+    <div className={cn(['card rounded-2xl bg-white dark:bg-dark-depth1 dark:text-grey dark:shadow-none w-full', className])}>
       {children}
     </div>
   )
